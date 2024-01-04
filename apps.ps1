0326fea3368a020ca7d13b6b57c4fd024ac618e8
Stian Thaulow <stian@thaulow.me>
2024-01-04 10:49:08 +0100

    Add loop and 7zip

--- apps.ps1
+++ apps.ps1
@@ -1,1 +1,16 @@
-winget install Microsoft.WindowsTerminal
+
+$apps = @(
+  @{name = "7zip.7zip" },
+  @{name = "Microsoft.WindowsTerminal" },
+)
+
+foreach ($app in $apps) {
+  $listApp = winget list --exact -q $app.name
+  if (-not $listApp -like "*$app.name*") {
+      Write-Host "Installing: $($app.name)"
+      winget install -e -h --accept-source-agreements --accept-package-agreements --id $app.name 
+  }
+  else {
+      Write-Host "Skipping: $($app.name) (already installed)"
+  }
+}
