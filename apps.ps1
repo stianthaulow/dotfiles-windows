15229a3ae6c7d970853ead53faa60c608e055f74
Stian Thaulow <stian@thaulow.me>
2024-01-04 10:52:36 +0100

    Update app installation logic

--- apps.ps1
+++ apps.ps1
@@ -1,16 +1,16 @@
 
 $apps = @(
-  @{name = "7zip.7zip" },
+  @{name = "7zip.7zi" },
   @{name = "Microsoft.WindowsTerminal" }
 )
 
 foreach ($app in $apps) {
   $listApp = winget list --exact -q $app.name
-  if (-not $listApp -like "*$app.name*") {
-      Write-Host "Installing: $($app.name)"
+  if (![String]::Join("", $listApp).Contains($app.name)) {
+      Write-host "Installing: " $app.name
       winget install -e -h --accept-source-agreements --accept-package-agreements --id $app.name 
   }
   else {
-      Write-Host "Skipping: $($app.name) (already installed)"
+      Write-host "Skipping: " $app.name " (already installed)"
   }
 }
