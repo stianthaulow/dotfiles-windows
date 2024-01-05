133eb537758fefd8f1f994ca6e356c7a4c877166
Stian Thaulow <stian@thaulow.me>
2024-01-05 15:01:55 +0100

    Add support for English language and set Norwegian locale

--- language.ps1
+++ language.ps1
@@ -1,4 +1,8 @@
 # Set norwegian keyboard layout
 $languageList = New-WinUserLanguageList -Language "nb-NO"
+$languageList.Add("en-US")
 $languageList[0].InputMethodTips.Add("0414:00000414")
 Set-WinUserLanguageList $languageList -Force
+
+Set-WinSystemLocale "nb-NO"
+Set-WinUILanguageOverride -Language "en-US"
