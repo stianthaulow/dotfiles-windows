948c92400f9fd30385ed1d50abf554c22b557a17
Stian Thaulow <stian@thaulow.me>
2024-01-05 10:42:12 +0100

    Refactor app installation logic

--- apps.ps1
+++ apps.ps1
@@ -1,45 +1,45 @@
 $apps = @(
-  @{Id = "JanDeDobbeleer.OhMyPosh"; Name = "Oh My Posh"},
-  @{Id = "7zip.7zip"; Name = "7zip"},
-  @{Id = "Microsoft.WindowsTerminal"; Name = "Windows Terminal"},
-  @{Id = "Google.Chrome"; Name = "Google Chrome"},
-  @{Id = "Neovim.Neovim"; Name = "Neovim"},
-  @{Id = "Microsoft.PowerToys"; Name = "PowerToys"},
-  @{Id = "Microsoft.VisualStudioCode"; Name = "Visual Studio Code"},
-  @{Id = "AutoHotkey.AutoHotkey"; Name = "AutoHotkey"},
-  @{Id = "Spotify.Spotify"; Name = "Spotify"},
-  @{Id = "Ditto.Ditto"; Name = "Ditto"},
-  @{Id = "GitHub.cli"; Name = "GitHub CLI"}
-  @{Id = "jqlang.jq"; Name = "jq (JSON CLI)"}
-  @{Id = "junegunn.fzf"; Name = "fzf (fuzzy finder)"}
-  @{Id = "Schniz.fnm"; Name = "Fast Node Manager (fnm)"},
-  @{Id = "Obsidian.Obsidian"; Name = "Obsidian"},
-  @{Id = "Notepad++.Notepad++"; Name = "Notepad++"},
-  @{Id = "Bitwarden.Bitwarden"; Name = "Bitwarden"},
-  @{Id = "DigitalScholar.Zotero"; Name = "Zotero"},
-  @{Id = "voidtools.Everything"; Name = "Everything search"},
-  @{Id = "Discord.Discord"; Name = "Discord"},
-  @{Id = "tailscale.tailscale"; Name = "Tailscale"},
-  @{Id = "NickeManarin.ScreenToGif"; Name = "ScreenToGif"}
-  @{Id = "WireGuard.WireGuard"; Name = "WireGuard"}
-  @{Id = "suse.RancherDesktop"; Name = "Rancher Desktop"}
-  @{Id = "Python.Python.3.10"; Name = "Python 3.10"}
-  @{Id = "Python.Python.3.12"; Name = "Python 3.12"}
-  @{Id = "Microsoft.DotNet.SDK.8"; Name = ".NET 8 SDK"}
+    @{Id = "JanDeDobbeleer.OhMyPosh"; Name = "Oh My Posh" },
+    @{Id = "7zip.7zip"; Name = "7zip" },
+    @{Id = "Microsoft.WindowsTerminal"; Name = "Windows Terminal" },
+    @{Id = "Google.Chrome"; Name = "Google Chrome" },
+    @{Id = "Neovim.Neovim"; Name = "Neovim" },
+    @{Id = "Microsoft.PowerToys"; Name = "PowerToys" },
+    @{Id = "Microsoft.VisualStudioCode"; Name = "Visual Studio Code"; Args = "--override '/SILENT /mergetasks=`"!runcode,addcontextmenufiles,addcontextmenufolders`"'" },
+    @{Id = "AutoHotkey.AutoHotkey"; Name = "AutoHotkey" },
+    @{Id = "Spotify.Spotify"; Name = "Spotify" },
+    @{Id = "Ditto.Ditto"; Name = "Ditto" },
+    @{Id = "GitHub.cli"; Name = "GitHub CLI" }
+    @{Id = "jqlang.jq"; Name = "jq (JSON CLI)" }
+    @{Id = "junegunn.fzf"; Name = "fzf (fuzzy finder)" }
+    @{Id = "Schniz.fnm"; Name = "Fast Node Manager (fnm)" },
+    @{Id = "Obsidian.Obsidian"; Name = "Obsidian" },
+    @{Id = "Notepad++.Notepad++"; Name = "Notepad++" },
+    @{Id = "Bitwarden.Bitwarden"; Name = "Bitwarden" },
+    @{Id = "DigitalScholar.Zotero"; Name = "Zotero" },
+    @{Id = "voidtools.Everything"; Name = "Everything search" },
+    @{Id = "Discord.Discord"; Name = "Discord" },
+    @{Id = "tailscale.tailscale"; Name = "Tailscale" },
+    @{Id = "NickeManarin.ScreenToGif"; Name = "ScreenToGif" }
+    @{Id = "WireGuard.WireGuard"; Name = "WireGuard" }
+    @{Id = "suse.RancherDesktop"; Name = "Rancher Desktop" }
+    @{Id = "Python.Python.3.10"; Name = "Python 3.10" }
+    @{Id = "Python.Python.3.12"; Name = "Python 3.12" }
+    @{Id = "Microsoft.DotNet.SDK.8"; Name = ".NET 8 SDK" }
 )
 
 function List($apps, $currentSelection, $selectedApps) {
-  for ($i = 0; $i -lt $apps.Count; $i++) {
-      if ($i -eq $currentSelection) {
-          Write-Host "-> $($apps[$i].Name)" -ForegroundColor Cyan
-      }
-      elseif ($selectedApps -contains $i) {
-          Write-Host "[x] $($apps[$i].Name)"
-      }
-      else {
-          Write-Host "[ ] $($apps[$i].Name)"
-      }
-  }
+    for ($i = 0; $i -lt $apps.Count; $i++) {
+        if ($i -eq $currentSelection) {
+            Write-Host "-> $($apps[$i].Name)" -ForegroundColor Cyan
+        }
+        elseif ($selectedApps -contains $i) {
+            Write-Host "[x] $($apps[$i].Name)"
+        }
+        else {
+            Write-Host "[ ] $($apps[$i].Name)"
+        }
+    }
 }
 
 $currentIndex = 0
@@ -88,10 +88,11 @@ If ($selectedApps.Count -ne 0) {
     foreach ($index in $selectedApps) {
         $appId = $apps[$index].Id
         $appName = $apps[$index].Name
+        $appArgs = $apps[$index].Args
         $listApp = winget list --exact -q $appId
         if (![String]::Join("", $listApp).Contains($appId)) {
             Write-host "Installing: " $appId
-            winget install -e -h --accept-source-agreements --accept-package-agreements --id $appId 
+            winget install -e -h --accept-source-agreements --accept-package-agreements --id $appId $appArgs
         }
         else {
             Write-host "Skipping: " $appName " (already installed)" -ForegroundColor DarkGray
