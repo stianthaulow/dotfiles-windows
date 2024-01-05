308475420e6919355321296bc679ddca738c2640
Stian Thaulow <stian@thaulow.me>
2024-01-05 15:38:52 +0100

    Add language and culture settings

--- language.ps1
+++ language.ps1
@@ -3,6 +3,6 @@ $languageList = New-WinUserLanguageList -Language "nb-NO"
 $languageList.Add("en-US")
 $languageList[0].InputMethodTips.Add("0414:00000414")
 Set-WinUserLanguageList $languageList -Force
-
+Set-Culture "nb-NO"
 Set-WinSystemLocale "nb-NO"
 Set-WinUILanguageOverride -Language "en-US"
